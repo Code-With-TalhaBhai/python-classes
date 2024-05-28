bc8f9a4c352b580cd31ca7ce384cb7a84af4b4ef
= <=>
2024-05-28 12:25:26 +0500

    finally diameter done

--- DSA/04_Trees/Problems/02_Diameter.py
+++ DSA/04_Trees/Problems/02_Diameter.py
@@ -17,17 +17,34 @@ def height(root):
     return hei
 
 
-def diameter(root):
+# Time Complexity(O(n^2)) -> using nodes
+def diameter1(root):
     if root == None:
         return 0
     
-    left = diameter(root.left)
-    right = diameter(root.right)
+    left = diameter1(root.left)
+    right = diameter1(root.right)
     left_right_height = height(root.left) + height(root.right) + 1
     
     dia = max(left,max(left_right_height,right))
-
     return dia
 
 
-print(diameter(root))
+# Time Complexity(O(n)) -> using edges
+def diameter2(root): 
+    def diameter(root):
+        if root is None:
+            return (-1,-1) # tuple
+        
+        left = diameter(root.left)
+        right = diameter(root.right)
+
+        # hei = max(left[0],right[0]) + 1
+        # dia = left[0]+right[0] + 2
+        # return (hei,dia)
+        return (max(left[0],right[0])+1,left[0]+right[0]+2)
+    return diameter(root)[1]
+
+
+print(diameter1(root))
+print(diameter2(root))
