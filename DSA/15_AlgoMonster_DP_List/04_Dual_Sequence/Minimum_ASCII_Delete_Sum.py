69aba60503357b45f02a79f9fa28684a82d17636
= <=>
2025-04-01 14:54:06 +0500

    Dual Sequence Completed

--- DSA/15_AlgoMonster_DP_List/04_Dual_Sequence/Minimum_ASCII_Delete_Sum.py
+++ DSA/15_AlgoMonster_DP_List/04_Dual_Sequence/Minimum_ASCII_Delete_Sum.py
@@ -40,24 +40,22 @@ def minimumDeleteSum2(s1,s2):
     dp = [[0 for i in range(m+1)] for i in range(n+1)]
 
     for i in range(1,m+1):
-        dp[0][i] = ord(s1[i-1])
+        dp[0][i] = ord(s1[i-1]) + dp[0][i-1]
 
     for j in range(1,n+1):
-        dp[j][0] = ord(s2[j-1])
+        dp[j][0] = ord(s2[j-1]) + dp[j-1][0]
 
     for i in range(1,n+1):
         for j in range(1,m+1):
-            if s1[j-1] != s2[i-1]:
-                dp[i][j] = dp[i][j-1] + dp[i-1][j]
+            if s1[j-1] == s2[i-1]:
+                dp[i][j] = dp[i-1][j-1]
             else:
-                dp[i][j] = dp[i][j-1] - dp[i-1][j]
-
-    print(dp)
+                dp[i][j] = min(dp[i][j-1]+ord(s1[j-1]),dp[i-1][j]+ord(s2[i-1]))
     return dp[n][m]
 
 
 print(minimumDeleteSum1('sea','eat'))
 print(minimumDeleteSum1('delete','leet'))
 print()
-# print(minimumDeleteSum2('sea','eat'))
-# print(minimumDeleteSum2('delete','leet'))
+print(minimumDeleteSum2('sea','eat'))
+print(minimumDeleteSum2('delete','leet'))
--- DSA/15_AlgoMonster_DP_List/04_Dual_Sequence/Minimum_Characters_Delete.py
+++ DSA/15_AlgoMonster_DP_List/04_Dual_Sequence/Minimum_Characters_Delete.py
@@ -31,8 +31,21 @@ def minimum_characters_delete2(s1,s2):
     m = len(s1)
     n = len(s2)
 
-    ...
+    dp = [[0 for i in range(m+1)] for i in range(n+1)]
 
+    for i in range(n+1):
+        dp[i][0] = i
+
+    for i in range(m+1):
+        dp[0][i] = i
+
+    for i in range(1,n+1):
+        for j in range(1,m+1):       
+            if s1[j-1] == s2[i-1]:
+                dp[i][j] = dp[i-1][j-1]
+            else:
+                dp[i][j] = min(dp[i-1][j],dp[i][j-1])+1
+    return dp[n][m]
 
 
 print(minimum_characters_delete1('sea','eat'))
